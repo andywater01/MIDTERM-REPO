63be627b02470e45115321653722b516145a97cc
Fardeen Faisal <fardeen.faisal@ontariotechu.net>
2020-10-27 19:13:22 -0400

    Added Reference for MeshFactory

--- samples/INFR-2310/Midterm/src/Original.cpp
+++ samples/INFR-2310/Midterm/src/Original.cpp
@@ -1,3 +1,4 @@
+/*
 #include <Logging.h>
 #include <iostream>
 
@@ -23,9 +24,6 @@
 #include "InputHelpers.h"
 #include "MeshBuilder.h"
 #include "MeshFactory.h"
-//Includes the obj loader header
-#include "ObjLoader.h"
-//Includes the not-obj header
 #include "NotObjLoader.h"
 #include "VertexTypes.h"
 
@@ -41,7 +39,7 @@
 	@param length    The length of the message
 	@param message   The human readable message from OpenGL
 	@param userParam The pointer we set with glDebugMessageCallback (should be the game pointer)
-*/
+
 void GlDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar* message, const void* userParam) {
 	std::string sourceTxt;
 	switch (source) {
@@ -56,18 +54,15 @@ void GlDebugMessage(GLenum source, GLenum type, GLuint id, GLenum severity, GLsi
 	case GL_DEBUG_SEVERITY_LOW:          LOG_INFO("[{}] {}", sourceTxt, message); break;
 	case GL_DEBUG_SEVERITY_MEDIUM:       LOG_WARN("[{}] {}", sourceTxt, message); break;
 	case GL_DEBUG_SEVERITY_HIGH:         LOG_ERROR("[{}] {}", sourceTxt, message); break;
-		#ifdef LOG_GL_NOTIFICATIONS
+#ifdef LOG_GL_NOTIFICATIONS
 	case GL_DEBUG_SEVERITY_NOTIFICATION: LOG_INFO("[{}] {}", sourceTxt, message); break;
-		#endif
+#endif
 	default: break;
 	}
 }
 
 GLFWwindow* window;
 Camera::sptr camera = nullptr;
-
-bool perspective = true;
-bool isPressed = false;
 
 void GlfwWindowResizedCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
@@ -83,7 +78,7 @@ bool initGLFW() {
 #ifdef _DEBUG
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
 #endif
-	
+
 	//Create a new GLFW window
 	window = glfwCreateWindow(800, 800, "INFR1350U", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
@@ -141,8 +136,6 @@ void ShutdownImGui()
 	ImGui::DestroyContext();
 }
 
-
-
 std::vector<std::function<void()>> imGuiCallbacks;
 void RenderImGui() {
 	// Implementation new frame
@@ -158,7 +151,7 @@ void RenderImGui() {
 		}
 		ImGui::End();
 	}
-	
+
 	// Make sure ImGui knows how big our window is
 	ImGuiIO& io = ImGui::GetIO();
 	int width{ 0 }, height{ 0 };
@@ -194,10 +187,6 @@ int main() {
 	glEnable(GL_DEBUG_OUTPUT);
 	glDebugMessageCallback(GlDebugMessage, nullptr);
 
-
-	
-	
-
 	static const float points[] = {
 		-0.5f, -0.5f, 0.1f,
 		 0.5f, -0.5f, 0.1f,
@@ -220,17 +209,17 @@ int main() {
 	VertexArrayObject::sptr vao = VertexArrayObject::Create();
 	vao->AddVertexBuffer(posVbo, {
 		BufferAttribute(0, 3, GL_FLOAT, false, 0, NULL)
-	});
+		});
 	vao->AddVertexBuffer(color_vbo, {
 		BufferAttribute(1, 3, GL_FLOAT, false, 0, NULL)
-	});
+		});
 
 	static const VertexPosCol interleaved[] = {
-    //     X      Y     Z       R     G    B
-		{{ 0.5f, -0.5f, 0.0f},   {0.0f, 0.0f, 0.0f, 1.0f}},
-		{{ 0.5f,  0.5f, 0.0f},  {0.3f, 0.2f, 0.5f, 1.0f}},
-	    {{-0.5f,  0.5f, 0.0f},  {1.0f, 1.0f, 0.0f, 1.0f}},
-		{{ 0.5f,  1.0f, 0.0f},  {1.0f, 1.0f, 1.0f, 1.0f}}
+		//     X      Y     Z       R     G    B
+			{{ 0.5f, -0.5f, 0.0f},   {0.0f, 0.0f, 0.0f, 1.0f}},
+			{{ 0.5f,  0.5f, 0.0f},  {0.3f, 0.2f, 0.5f, 1.0f}},
+			{{-0.5f,  0.5f, 0.0f},  {1.0f, 1.0f, 0.0f, 1.0f}},
+			{{ 0.5f,  1.0f, 0.0f},  {1.0f, 1.0f, 1.0f, 1.0f}}
 	};
 
 	VertexBuffer::sptr interleaved_vbo = VertexBuffer::Create();
@@ -248,26 +237,8 @@ int main() {
 	vao2->AddVertexBuffer(interleaved_vbo, VertexPosCol::V_DECL);
 	vao2->SetIndexBuffer(interleaved_ibo);
 
-	/////////////// VertexPosNormTex Stuff ///////////////
-
-	/*std::vector<VertexPosNormTex> verticies;
-	std::vector<glm::vec3> positions;
-	std::vector<glm::vec3> normals;
-	std::vector<glm::vec3> uvs;*/
-
-	// f p1/t1/n1 p2/t2/n2 p3/t3/n3
-	/*VertexPosNormTex v1 = VertexPosNormTex(positions[vertices[i] - 1], uvs [t1 - 1], normals[n1 - 1]);
-	VertexPosNormTex v2 = VertexPosNormTex(positions[p2 - 1], uvs [t2 - 1], normals[n2 - 1]);
-	VertexPosNormTex v3 = VertexPosNormTex(positions[p3 - 1], uvs [t3 - 1], normals[n3 - 1]);*/
-
-	/*verticies.push_back(v1);
-	verticies.push_back(v2);
-	verticies.push_back(v3);*/
-
-
-
 	////////////// NEW STUFF
-	
+
 	// We'll use the provided mesh builder to build a new mesh with a few elements
 	MeshBuilder<VertexPosNormTexCol> builder = MeshBuilder<VertexPosNormTexCol>();
 	MeshFactory::AddPlane(builder, glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(1.0f, 0.0, 0.0f), glm::vec2(100.0f, 100.0f), glm::vec4(1.0f));
@@ -280,48 +251,22 @@ int main() {
 	// load an exact copy of the mesh created above
 	VertexArrayObject::sptr vao4 = NotObjLoader::LoadFromFile("Sample.notobj");
 
-	//Testobj.txt
-	std::vector<glm::vec3> positions;
-	std::vector<glm::vec3> normals;
-	std::vector<glm::vec2> uvs;
-
-	VertexArrayObject::sptr theVAO = nullptr;
-	bool loader = ObjLoader::LoadFromFile("SkyBoundCharacter2.obj", positions, uvs, normals);
-
-	theVAO = VertexArrayObject::Create();
-	VertexBuffer::sptr vertices = VertexBuffer::Create();
-	vertices->LoadData(positions.data(), positions.size());
-
-	VertexBuffer::sptr _normals = VertexBuffer::Create();
-	_normals->LoadData(normals.data(), normals.size());
-
-	theVAO = VertexArrayObject::Create();
-
-	theVAO->AddVertexBuffer(vertices, {
-		BufferAttribute(0, 3, GL_FLOAT, false, 0, NULL)
-		});
-	theVAO->AddVertexBuffer(_normals, {
-		BufferAttribute(2, 3, GL_FLOAT, false, 0, NULL)
-		});
-
-
 	// Load our shaders
 	Shader::sptr shader = Shader::Create();
 	shader->LoadShaderPartFromFile("shaders/vertex_shader.glsl", GL_VERTEX_SHADER);
-	shader->LoadShaderPartFromFile("shaders/frag_blinn_phong.glsl", GL_FRAGMENT_SHADER);  
-	shader->Link();  
+	shader->LoadShaderPartFromFile("shaders/frag_blinn_phong.glsl", GL_FRAGMENT_SHADER);
+	shader->Link();
 
 	glm::vec3 lightPos = glm::vec3(0.0f, 0.0f, 2.0f);
-	glm::vec3 lightCol = glm::vec3(1.0f, 30.0f, 1.0f);
+	glm::vec3 lightCol = glm::vec3(0.3f, 0.2f, 0.5f);
 	float     lightAmbientPow = 0.05f;
-	float     lightSpecularPow = 10.0f;
-	glm::vec3 ambientCol = glm::vec3(30.0f);
-	float     ambientPow = 1.0f;
-	float     shininess = 1.2f;
+	float     lightSpecularPow = 1.0f;
+	glm::vec3 ambientCol = glm::vec3(1.0f);
+	float     ambientPow = 0.1f;
+	float     shininess = 4.0f;
 	// These are our application / scene level uniforms that don't necessarily update
 	// every frame
 	shader->SetUniform("u_LightPos", lightPos);
-	shader->SetUniform("inColor", glm::vec3(1.0f));
 	shader->SetUniform("u_LightCol", lightCol);
 	shader->SetUniform("u_AmbientLightStrength", lightAmbientPow);
 	shader->SetUniform("u_SpecularLightStrength", lightSpecularPow);
@@ -337,10 +282,10 @@ int main() {
 				shader->SetUniform("u_AmbientCol", ambientCol);
 			}
 			if (ImGui::SliderFloat("Fixed Ambient Power", &ambientPow, 0.01f, 1.0f)) {
-				shader->SetUniform("u_AmbientStrength", ambientPow); 
+				shader->SetUniform("u_AmbientStrength", ambientPow);
 			}
 		}
-		if (ImGui::CollapsingHeader("Light Level Lighting Settings")) 
+		if (ImGui::CollapsingHeader("Light Level Lighting Settings"))
 		{
 			if (ImGui::SliderFloat3("Light Pos", glm::value_ptr(lightPos), -10.0f, 10.0f)) {
 				shader->SetUniform("u_LightPos", lightPos);
@@ -361,30 +306,22 @@ int main() {
 				shader->SetUniform("u_Shininess", shininess);
 			}
 		}
-	});
+		});
 
 	// GL states
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
-	glm::mat4 transform = glm::mat4(0.8f);
+	glm::mat4 transform = glm::mat4(1.0f);
 	glm::mat4 transform2 = glm::mat4(1.0f);
 	glm::mat4 transform3 = glm::mat4(1.0f);
-	glm::mat4 transform4 = glm::mat4(1.2f);
-
-
 
 	camera = Camera::Create();
-
-	
-
-	
-
-	camera->SetPosition(glm::vec3(0, 1, 10)); // Set initial position
+	camera->SetPosition(glm::vec3(0, 3, 3)); // Set initial position
 	camera->SetUp(glm::vec3(0, 0, 1)); // Use a z-up coordinate system
 	camera->LookAt(glm::vec3(0.0f)); // Look at center of the screen
 	camera->SetFovDegrees(90.0f); // Set an initial FOV
-	
+
 	// This is an example of a key press handling helper. Look at InputHelpers.h an .cpp to see
 	// how this is implemented. Note that the ampersand here is capturing the variables within
 	// the scope. If you wanted to do some method on the class, your best bet would be to give it a method and
@@ -393,13 +330,13 @@ int main() {
 	KeyPressWatcher tKeyWatcher = KeyPressWatcher(GLFW_KEY_T, [&]() {
 		is_wireframe = !is_wireframe;
 		glPolygonMode(GL_FRONT, is_wireframe ? GL_LINE : GL_FILL);
-	});
+		});
 
 	InitImGui();
-		
+
 	// Our high-precision timer
 	double lastFrame = glfwGetTime();
-	
+
 	///// Game loop /////
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -412,10 +349,7 @@ int main() {
 		tKeyWatcher.Poll(window);
 
 		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-			transform3 = glm::translate(transform3, glm::vec3( 1.0f * dt, 0.0f, 0.0f));
-			//camera->SetPosition(camera->GetPosition() + glm::vec3());
-			//printf("Cam Test\n");
-			
+			transform3 = glm::translate(transform3, glm::vec3(1.0f * dt, 0.0f, 0.0f));
 		}
 		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
 			transform3 = glm::translate(transform3, glm::vec3(-1.0f * dt, 0.0f, 0.0f));
@@ -424,50 +358,35 @@ int main() {
 			transform3 = glm::translate(transform3, glm::vec3(0.0f, -1.0f * dt, 0.0f));
 		}
 		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-			transform3 = glm::translate(transform3, glm::vec3(1.0f,  1.0f * dt, 0.0f));
+			transform3 = glm::translate(transform3, glm::vec3(0.0f, 1.0f * dt, 0.0f));
 		}
-		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS && isPressed == false)
-		{
-			isPressed = true;
-			perspective = !perspective;
-			camera->SetProjectionType(perspective);
-			//camera->GetViewProjection();
-		}
-		else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE && isPressed == true)
-		{
-			isPressed = false;
-			
-		}
-				
-		transform = glm::rotate_slow(glm::mat4(1.0f), static_cast<float>(thisFrame), glm::vec3(0, 1, 0));
+
+		transform = glm::rotate(glm::mat4(1.0f), static_cast<float>(thisFrame), glm::vec3(0, 1, 0));
 		transform2 = transform * glm::translate(glm::mat4(1.0f), glm::vec3(0, 0.0f, glm::sin(static_cast<float>(thisFrame))));
 
-		//transform4 =  glm::translate(glm::mat4(1.0f), glm::vec3(3, 0.0f, glm::sin(static_cast<float>(thisFrame))));
-		transform4 = glm::rotate_slow(glm::mat4(1.0f), static_cast<float>(thisFrame), glm::vec3(0, -1, 0));
-		
-		transform4 = transform4 * glm::translate(glm::mat4(1.0f), glm::vec3(3, 0.0f, glm::sin(static_cast<float>(thisFrame))));
-		
 		glClearColor(0.08f, 0.17f, 0.31f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		
 
 		shader->Bind();
 		// These are the uniforms that update only once per frame
 		//shader->SetUniformMatrix("u_View", camera->GetView());
 		shader->SetUniform("u_CamPos", camera->GetPosition());
 
-		
 		// These uniforms update for every object we want to draw
 		shader->SetUniformMatrix("u_ModelViewProjection", camera->GetViewProjection() * transform);
 		shader->SetUniformMatrix("u_Model", transform);
 		shader->SetUniformMatrix("u_ModelRotation", glm::mat3(transform));
-		theVAO->Render();
+		vao->Render();
 
+		shader->SetUniformMatrix("u_ModelViewProjection", camera->GetViewProjection() * transform2);
+		shader->SetUniformMatrix("u_Model", transform2);
+		shader->SetUniformMatrix("u_ModelRotation", glm::mat3(transform2));
+		vao2->Render();
 
-
-		
-		
+		shader->SetUniformMatrix("u_ModelViewProjection", camera->GetViewProjection() * transform3);
+		shader->SetUniformMatrix("u_Model", transform3);
+		shader->SetUniformMatrix("u_ModelRotation", glm::mat3(transform3));
+		vao4->Render();
 
 		RenderImGui();
 
@@ -481,3 +400,4 @@ int main() {
 	Logger::Uninitialize();
 	return 0;
 }
+*/
